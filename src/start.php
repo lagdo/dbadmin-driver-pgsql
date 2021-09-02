4b22afa75eee61c3accc0e07995a908310afb3cf
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-02 11:08:32 +0200

    Improved DI definitions.

--- src/start.php
+++ src/start.php
@@ -2,9 +2,5 @@
 
 $di = \jaxon()->di();
 // Register the database classes in the dependency container
-$di->set(Lagdo\DbAdmin\Driver\PgSql\Server::class, function($di) {
-    return new Lagdo\DbAdmin\Driver\PgSql\Server(
-        $di->get(Lagdo\Adminer\Driver\DbInterface::class),
-        $di->get(Lagdo\Adminer\Driver\UtilInterface::class));
-});
+$di->auto(Lagdo\DbAdmin\Driver\PgSql\Server::class);
 $di->alias('adminer_server_pgsql', Lagdo\DbAdmin\Driver\PgSql\Server::class);
