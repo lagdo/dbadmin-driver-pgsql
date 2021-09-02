9015bb92542d71b394ef4be428c7e8cf299ce395
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-02 06:34:47 +0200

    Use alias in di definitions.

--- src/start.php
+++ src/start.php
@@ -1,8 +1,10 @@
 <?php
 
+$di = \jaxon()->di();
 // Register the database classes in the dependency container
-\jaxon()->di()->set('adminer_server_pgsql', function($di) {
+$di->set(Lagdo\DbAdmin\Driver\PgSql\Server::class, function($di) {
     return new Lagdo\DbAdmin\Driver\PgSql\Server(
         $di->get(Lagdo\Adminer\Driver\DbInterface::class),
         $di->get(Lagdo\Adminer\Driver\UtilInterface::class));
 });
+$di->alias('adminer_server_pgsql', Lagdo\DbAdmin\Driver\PgSql\Server::class);
