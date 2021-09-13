eceb79b28b71dd3e8ef113315fd5a170daf1c583
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-13 02:36:20 +0200

    Moved translation feature to a new interface.

--- src/Driver.php
+++ src/Driver.php
@@ -24,22 +24,22 @@ class Driver extends AbstractDriver
     {
         $connection = null;
         if (extension_loaded("pgsql")) {
-            $connection = new Db\PgSql\Connection($this, $this->util, 'PgSQL');
+            $connection = new Db\PgSql\Connection($this, $this->util, $this->trans, 'PgSQL');
         }
         elseif (extension_loaded("pdo_pgsql")) {
-            $connection = new Db\Pdo\Connection($this, $this->util, 'PDO_PgSQL');
+            $connection = new Db\Pdo\Connection($this, $this->util, $this->trans, 'PDO_PgSQL');
         }
         else {
-            throw new AuthException($this->util->lang('No package installed to connect to a PostgreSQL server.'));
+            throw new AuthException($this->trans->lang('No package installed to connect to a PostgreSQL server.'));
         }
 
         $firstConnection = ($this->connection === null);
         if ($firstConnection) {
             $this->connection = $connection;
-            $this->server = new Db\Server($this, $this->util, $connection);
-            $this->table = new Db\Table($this, $this->util, $connection);
-            $this->query = new Db\Query($this, $this->util, $connection);
-            $this->grammar = new Db\Grammar($this, $this->util, $connection);
+            $this->server = new Db\Server($this, $this->util, $this->trans, $connection);
+            $this->table = new Db\Table($this, $this->util, $this->trans, $connection);
+            $this->query = new Db\Query($this, $this->util, $this->trans, $connection);
+            $this->grammar = new Db\Grammar($this, $this->util, $this->trans, $connection);
         }
 
         if (!$connection->open($this->options('server'), $this->options())) {
@@ -50,10 +50,10 @@ class Driver extends AbstractDriver
             $connection->query("SET application_name = 'Adminer'");
         }
         if ($firstConnection && $this->minVersion(9.2, 0)) {
-            $this->config->structuredTypes[$this->util->lang('Strings')][] = "json";
+            $this->config->structuredTypes[$this->trans->lang('Strings')][] = "json";
             $this->config->types["json"] = 4294967295;
             if ($this->minVersion(9.4, 0)) {
-                $this->config->structuredTypes[$this->util->lang('Strings')][] = "jsonb";
+                $this->config->structuredTypes[$this->trans->lang('Strings')][] = "jsonb";
                 $this->config->types["jsonb"] = 4294967295;
             }
         }
@@ -73,7 +73,7 @@ class Driver extends AbstractDriver
         foreach ($this->userTypes() as $type) { //! get types from current_schemas('t')
             if (!isset($this->config->types[$type])) {
                 $this->config->types[$type] = 0;
-                $this->config->structuredTypes[$this->util->lang('User types')][] = $type;
+                $this->config->structuredTypes[$this->trans->lang('User types')][] = $type;
             }
         }
         return $return;
@@ -98,12 +98,12 @@ class Driver extends AbstractDriver
         $this->config->drivers = ["PgSQL", "PDO_PgSQL"];
 
         $groups = [ //! arrays
-            $this->util->lang('Numbers') => ["smallint" => 5, "integer" => 10, "bigint" => 19, "boolean" => 1, "numeric" => 0, "real" => 7, "double precision" => 16, "money" => 20],
-            $this->util->lang('Date and time') => ["date" => 13, "time" => 17, "timestamp" => 20, "timestamptz" => 21, "interval" => 0],
-            $this->util->lang('Strings') => ["character" => 0, "character varying" => 0, "text" => 0, "tsquery" => 0, "tsvector" => 0, "uuid" => 0, "xml" => 0],
-            $this->util->lang('Binary') => ["bit" => 0, "bit varying" => 0, "bytea" => 0],
-            $this->util->lang('Network') => ["cidr" => 43, "inet" => 43, "macaddr" => 17, "txid_snapshot" => 0],
-            $this->util->lang('Geometry') => ["box" => 0, "circle" => 0, "line" => 0, "lseg" => 0, "path" => 0, "point" => 0, "polygon" => 0],
+            $this->trans->lang('Numbers') => ["smallint" => 5, "integer" => 10, "bigint" => 19, "boolean" => 1, "numeric" => 0, "real" => 7, "double precision" => 16, "money" => 20],
+            $this->trans->lang('Date and time') => ["date" => 13, "time" => 17, "timestamp" => 20, "timestamptz" => 21, "interval" => 0],
+            $this->trans->lang('Strings') => ["character" => 0, "character varying" => 0, "text" => 0, "tsquery" => 0, "tsvector" => 0, "uuid" => 0, "xml" => 0],
+            $this->trans->lang('Binary') => ["bit" => 0, "bit varying" => 0, "bytea" => 0],
+            $this->trans->lang('Network') => ["cidr" => 43, "inet" => 43, "macaddr" => 17, "txid_snapshot" => 0],
+            $this->trans->lang('Geometry') => ["box" => 0, "circle" => 0, "line" => 0, "lseg" => 0, "path" => 0, "point" => 0, "polygon" => 0],
         ];
         foreach ($groups as $name => $types) {
             $this->config->structuredTypes[$name] = array_keys($types);
--- src/start.php
+++ src/start.php
@@ -4,7 +4,8 @@ $di = \jaxon()->di();
 // Register the database classes in the dependency container
 $di->set(Lagdo\DbAdmin\Driver\PgSql\Driver::class, function($di) {
     $util = $di->get(Lagdo\DbAdmin\Driver\UtilInterface::class);
+    $trans = $di->get(Lagdo\DbAdmin\Driver\TranslatorInterface::class);
     $options = $di->get('adminer_config_options');
-    return new Lagdo\DbAdmin\Driver\PgSql\Driver($util, $options);
+    return new Lagdo\DbAdmin\Driver\PgSql\Driver($util, $trans, $options);
 });
 $di->alias('adminer_driver_pgsql', Lagdo\DbAdmin\Driver\PgSql\Driver::class);
