eb07f620cd6eb6056b235bbc97172f97d747e752
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-01-21 03:49:01 +0100

    Code improvement.

--- src/Db/DatabaseTrait.php
+++ src/Db/DatabaseTrait.php
@@ -3,12 +3,56 @@
 namespace Lagdo\DbAdmin\Driver\PgSql\Db;
 
 use Lagdo\DbAdmin\Driver\Entity\TableEntity;
-use Lagdo\DbAdmin\Driver\Entity\RoutineEntity;
 
-use Lagdo\DbAdmin\Driver\Db\Database as AbstractDatabase;
+use function substr;
+use function implode;
+use function array_merge;
+use function strtoupper;
+use function array_map;
 
 trait DatabaseTrait
 {
+    /**
+     * @param TableEntity $tableAttrs
+     * @param array $queries
+     *
+     * @return void
+     */
+    private function _getRenameColumnQueries(TableEntity $tableAttrs, array &$queries)
+    {
+        foreach ($tableAttrs->edited as $field) {
+            $column = $this->driver->escapeId($field[0]);
+            $val = $field[1];
+            $val5 = $val[5] ?? '';
+            if ($val[0] !== '' && $column !== $val[0]) {
+                $queries[] = 'ALTER TABLE ' . $this->driver->table($tableAttrs->name) . " RENAME $column TO $val[0]";
+            }
+            if ($column !== '' || $val5 !== '') {
+                $queries[] = 'COMMENT ON COLUMN ' . $this->driver->table($tableAttrs->name) .
+                    ".$val[0] IS " . ($val5 !== '' ? substr($val5, 9) : "''");
+            }
+        }
+    }
+
+    /**
+     * @param TableEntity $tableAttrs
+     * @param array $queries
+     *
+     * @return void
+     */
+    private function _getColumnCommentQueries(TableEntity $tableAttrs, array &$queries)
+    {
+        foreach ($tableAttrs->fields as $field) {
+            $column = $this->driver->escapeId($field[0]);
+            $val = $field[1];
+            $val5 = $val[5] ?? '';
+            if ($column !== '' || $val5 !== '') {
+                $queries[] = 'COMMENT ON COLUMN ' . $this->driver->table($tableAttrs->name) .
+                    ".$val[0] IS " . ($val5 !== '' ? substr($val5, 9) : "''");
+            }
+        }
+    }
+
     /**
      * Get queries to create or alter table.
      *
@@ -16,32 +60,13 @@ trait DatabaseTrait
      *
      * @return array
      */
-    private function getQueries(TableEntity $tableAttrs)
+    private function getQueries(TableEntity $tableAttrs): array
     {
         $queries = [];
 
-        foreach ($tableAttrs->edited as $field) {
-            $column = $this->driver->escapeId($field[0]);
-            $val = $field[1];
-            $val5 = $val[5] ?? '';
-            if ($val[0] !== '' && $column != $val[0]) {
-                $queries[] = 'ALTER TABLE ' . $this->driver->table($tableAttrs->name) . " RENAME $column TO $val[0]";
-            }
-            if ($column !== '' || $val5 !== '') {
-                $queries[] = 'COMMENT ON COLUMN ' . $this->driver->table($tableAttrs->name) .
-                    ".$val[0] IS " . ($val5 != '' ? substr($val5, 9) : "''");
-            }
-        }
-        foreach ($tableAttrs->fields as $field) {
-            $column = $this->driver->escapeId($field[0]);
-            $val = $field[1];
-            $val5 = $val[5] ?? '';
-            if ($column !== '' || $val5 !== '') {
-                $queries[] = 'COMMENT ON COLUMN ' . $this->driver->table($tableAttrs->name) .
-                    ".$val[0] IS " . ($val5 != '' ? substr($val5, 9) : "''");
-            }
-        }
-        if ($tableAttrs->comment != '') {
+        $this->_getRenameColumnQueries($tableAttrs, $queries);
+        $this->_getColumnCommentQueries($tableAttrs, $queries);
+        if ($tableAttrs->comment !== '') {
             $queries[] = 'COMMENT ON TABLE ' . $this->driver->table($tableAttrs->name) .
                 ' IS ' . $this->driver->quote($tableAttrs->comment);
         }
@@ -56,14 +81,14 @@ trait DatabaseTrait
      *
      * @return array
      */
-    private function getNewColumns(TableEntity $tableAttrs)
+    private function getNewColumns(TableEntity $tableAttrs): array
     {
         $columns = [];
 
         foreach ($tableAttrs->fields as $field) {
             $val = $field[1];
             if (isset($val[6])) { // auto increment
-                $val[1] = ($val[1] == ' bigint' ? ' big' : ($val[1] == ' smallint' ? ' small' : ' ')) . 'serial';
+                $val[1] = ($val[1] === ' bigint' ? ' big' : ($val[1] === ' smallint' ? ' small' : ' ')) . 'serial';
             }
             $columns[] = implode($val);
             if (isset($val[6])) {
@@ -75,35 +100,57 @@ trait DatabaseTrait
     }
 
     /**
-     * Get queries to create or alter table.
-     *
      * @param TableEntity $tableAttrs
+     * @param array $columns
      *
-     * @return array
+     * @return void
      */
-    private function getColumnChanges(TableEntity $tableAttrs)
+    private function _getCreateColumnQueries(TableEntity $tableAttrs, array &$columns)
     {
-        $columns = [];
-
         foreach ($tableAttrs->fields as $field) {
             $val = $field[1];
             if (isset($val[6])) { // auto increment
-                $val[1] = ($val[1] == ' bigint' ? ' big' : ($val[1] == ' smallint' ? ' small' : ' ')) . 'serial';
+                $val[1] = ($val[1] === ' bigint' ? ' big' : ($val[1] === ' smallint' ? ' small' : ' ')) . 'serial';
             }
             $columns[] = 'ADD ' . implode($val);
             if (isset($val[6])) {
                 $columns[] = "ADD PRIMARY KEY ($val[0])";
             }
         }
+    }
+
+    /**
+     * @param TableEntity $tableAttrs
+     * @param array $columns
+     *
+     * @return void
+     */
+    private function _getAlterColumnQueries(TableEntity $tableAttrs, array &$columns)
+    {
         foreach ($tableAttrs->edited as $field) {
             $column = $this->driver->escapeId($field[0]);
             $val = $field[1];
             $columns[] = "ALTER $column TYPE$val[1]";
             if (!$val[6]) {
                 $columns[] = "ALTER $column " . ($val[3] ? "SET$val[3]" : 'DROP DEFAULT');
-                $columns[] = "ALTER $column " . ($val[2] == ' NULL' ? 'DROP NOT' : 'SET') . $val[2];
+                $columns[] = "ALTER $column " . ($val[2] === ' NULL' ? 'DROP NOT' : 'SET') . $val[2];
             }
         }
+    }
+
+    /**
+     * Get queries to create or alter table.
+     *
+     * @param TableEntity $tableAttrs
+     *
+     * @return array
+     */
+    private function getColumnChanges(TableEntity $tableAttrs): array
+    {
+        $columns = [];
+
+        $this->_getCreateColumnQueries($tableAttrs, $columns);
+        $this->_getAlterColumnQueries($tableAttrs, $columns);
         foreach ($tableAttrs->dropped as $column) {
             $columns[] = 'DROP ' . $this->driver->escapeId($column);
         }
